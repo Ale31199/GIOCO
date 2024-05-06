0cdc92bc0b0cd6bff4240f9e9f4f8ae67368a5eb
Alessio Santillo <alessiosantillo31199@gmail.com>
2024-05-06 16:10:03 +0200

    aggiunta build nuova

--- gioco/.netlify/server/manifest.js
+++ gioco/.netlify/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.qDgkKWnB.js","app":"_app/immutable/entry/app.cJi6S2h0.js","imports":["_app/immutable/entry/start.qDgkKWnB.js","_app/immutable/chunks/scheduler.V7bLW0bx.js","_app/immutable/chunks/singletons.mF-LWELI.js","_app/immutable/entry/app.cJi6S2h0.js","_app/immutable/chunks/scheduler.V7bLW0bx.js","_app/immutable/chunks/index.QOqH-36i.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.C1P8XlOT.js","app":"_app/immutable/entry/app.gZ3BqbqF.js","imports":["_app/immutable/entry/start.C1P8XlOT.js","_app/immutable/chunks/scheduler.V7bLW0bx.js","_app/immutable/chunks/singletons.MqXPwefs.js","_app/immutable/entry/app.gZ3BqbqF.js","_app/immutable/chunks/scheduler.V7bLW0bx.js","_app/immutable/chunks/index.QOqH-36i.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- gioco/.netlify/server/nodes/1.js
+++ gioco/.netlify/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1._l_XM7wL.js","_app/immutable/chunks/scheduler.V7bLW0bx.js","_app/immutable/chunks/index.QOqH-36i.js","_app/immutable/chunks/singletons.mF-LWELI.js"];
+export const imports = ["_app/immutable/nodes/1.Nn07AixU.js","_app/immutable/chunks/scheduler.V7bLW0bx.js","_app/immutable/chunks/index.QOqH-36i.js","_app/immutable/chunks/singletons.MqXPwefs.js"];
 export const stylesheets = [];
 export const fonts = [];
